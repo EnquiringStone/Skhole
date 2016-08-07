ec0ed1d20aef132e6a726a45fc360fdaf9e75de6
Johan Langes <johan-langes@hotmail.com>
2016-08-07 20:04:32 +0200

    Some small things

--- web/js/my-courses/my.courses.js
+++ web/js/my-courses/my.courses.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
     var body = $('body');
+
+    body.on('click', '.btn-pref .btn', function() {
+        var modal = $($(this).parents('.btn-pref'));
+        $(".btn", modal).removeClass("btn-primary").addClass("btn-default");
+        $(this).removeClass("btn-default").addClass("btn-primary");
+    });
+
     body.on('click', '.panel-body .pagination a', function(event) {
         updateReviewModals(this);
     });
